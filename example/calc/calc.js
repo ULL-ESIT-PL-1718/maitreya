58e12a3630e91cd1a4ef48ffd787e23d69faf593
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-12-20 11:31:35 +0000

    calc.js

--- example/calc/calc.js
+++ example/calc/calc.js
@@ -1,12 +1,15 @@
-let {defineGrammar, def, ref, many1, regex, choice} = require('maitreya/lib/grammar');
+let {defineGrammar, def, ref, many1, regex, choice, string} = require('maitreya/lib/grammar');
 let {GLRParser} = require('maitreya/lib/interpret');
 
 let grammar = defineGrammar(() => {
     def('exp', [ref('num')], ([num]) => num);
+    def('exp', [string('('), ref('exp'), string(')')], ([left, exp, right]) => exp);
     def('exp', [ref('exp'), ref('op'), ref('exp')], ([lhs, op, rhs]) => op(lhs, rhs));
     def('num', [many1(regex(/^[0-9]/))], ([digits]) => Number(digits.join('')));
-    def('op', [choice('+', '-')], ([op]) => {
+    def('op', [choice('*', '+', '-')], ([op]) => {
         return {
+            ['*'](lhs, rhs) { return lhs * rhs; },
+            ['/'](lhs, rhs) { return lhs / rhs; },
             ['+'](lhs, rhs) { return lhs + rhs; },
             ['-'](lhs, rhs) { return lhs - rhs; }
         }[op];
@@ -14,5 +17,5 @@ let grammar = defineGrammar(() => {
 });
 
 let parser = new GLRParser(grammar);
-parser.feed('3+2-5');
+parser.feed('7-(3-1)');
 console.log(parser.results);
